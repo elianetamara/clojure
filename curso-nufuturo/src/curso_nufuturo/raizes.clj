274fdedd53f0a0af2abadc6d26baa6d0a8368f05
elianetamara <eliane.tamara.lima.oliveira@ccc.ufcg.edu.br>
2024-03-10 19:40:12 -0300

    lint: formata codigo

--- curso-nufuturo/src/curso_nufuturo/raizes.clj
+++ curso-nufuturo/src/curso_nufuturo/raizes.clj
@@ -2,16 +2,16 @@
 (require '[clojure.math :as math])
 
 (defn delta [a b c]
-      (- (* b b) (* 4 a c)))
+  (- (* b b) (* 4 a c)))
 
 (defn raiz [b a raizd]
-      (/ (+ (* b -1) raizd) (* 2 a)))
+  (/ (+ (* b -1) raizd) (* 2 a)))
 
 (defn raizes [a b c]
-      (let [d (delta a b c) raizd (math/sqrt d)]
-           (cond (= d 0) [(raiz b a raizd)]
-                 (> d 0) [(raiz b a raizd), (raiz b a (* -1 raizd))]
-                 :else (println "delta negativo"))))
+  (let [d (delta a b c) raizd (math/sqrt d)]
+    (cond (= d 0) [(raiz b a raizd)]
+          (> d 0) [(raiz b a raizd), (raiz b a (* -1 raizd))]
+          :else (println "delta negativo"))))
 
 
 (println (raizes (read) (read) (read)))
